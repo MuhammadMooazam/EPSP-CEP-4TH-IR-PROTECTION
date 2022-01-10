2f8383444c40e04183f3d4b956648bcc9c540ea4
Muhammad Mooazam <muhammadmoazam29@gmail.com>
2022-01-11 01:07:59 +0500

    add files

--- firebase_get_post_nodemcu/firebase_get_post_nodemcu.ino
+++ firebase_get_post_nodemcu/firebase_get_post_nodemcu.ino
@@ -4,7 +4,6 @@
 #include <Hash.h>
 #include <FS.h>
 #include <ESPAsyncWebServer.h>
-#include <ThingSpeak.h>
 #include <math.h>
 #include "CComplex.h"
 
--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>ELECTRICAL POWER SYSTEM PROTECTION</h1>
+      <h2>FOURTH INDUSTRIAL REVOLUTION PROTECTION SYSTEM</h2>
       <h2>SENDING FAULT VALUES</h2>
       <SendData />
       <h2>GETTING FAULT TYPE</h2>
